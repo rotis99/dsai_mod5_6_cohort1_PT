95d337bf94ad9b63ab10dbadf32efbd5d73b3a94
TK Choi <taekchoi@gmail.com>
2025-05-23 16:01:47 +0000

    1

--- app.py
+++ app.py
@@ -3,16 +3,37 @@
 from flask import Flask,request,render_template
 import google.generativeai as genai
 import os
+import datetime
+import sqlite3
 
 gemini_api_key = os.getenv("gemini_api_key")
+
 genai.configure(api_key=gemini_api_key)
 model = genai.GenerativeModel("gemini-2.0-flash")
 
 app = Flask(__name__)
 
+first_time = 1
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    global first_time
+    if first_time==1:
+        q = request.form.get("q")
+        print(q)
+        t = datetime.datetime.now()
+        conn = sqlite3.connect('user.db')
+        c = conn.cursor()
+        c.execute("insert into users(name,timestamp) values(?,?)",(q,t))
+        conn.commit()
+        c.close()
+        conn.close()
+        first_time=0
+    return(render_template("main.html"))
 
 @app.route("/gemini",methods=["GET","POST"])
 def gemini():
@@ -23,8 +44,41 @@ def gemini_reply():
     q = request.form.get("q")
     print(q)
     r = model.generate_content(q)
-    r = r.text
-    return(render_template("gemini_reply.html",r=r))
+    return(render_template("gemini_reply.html",r=r.text))
+
+@app.route("/paynow",methods=["GET","POST"])
+def paynow():
+    return(render_template("paynow.html"))
+
+@app.route("/user_log",methods=["GET","POST"])
+def user_log():
+    #read
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("select * from users")
+    r=""
+    for row in c:
+        print(row)
+        r= r+str(row)
+    c.close()
+    conn.close()
+    return(render_template("user_log.html",r=r))
+
+@app.route("/delete_log",methods=["GET","POST"])
+def delete_log():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("delete from users")
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("delete_log.html"))
+
+@app.route("/logout",methods=["GET","POST"])
+def logout():
+    global first_time
+    first_time = 1
+    return(render_template("index.html"))
 
 if __name__ == "__main__":
     app.run()
